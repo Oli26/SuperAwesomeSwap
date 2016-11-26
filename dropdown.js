e27ec3bfdfa29220514dc035e24836cd52afc9b6
tolgaparlan <tolgaparlan@gmail.com>
2016-11-26 18:12:46 +0100

    added difficulty passing

--- dropdown.js
+++ dropdown.js
@@ -1,12 +1,30 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
+
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function yourFunction(){
+    var v1 = getParameterByName("diff");
+    window.alert(v1);
+}
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.dropbtn')) {
 
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
